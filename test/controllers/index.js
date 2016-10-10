c7c274e0cd1d37902a18348ae98274a9e62a6ab1
Ming Yang <mgy209@nyu.edu>
2016-10-10 22:43:36 +0000

    Updated all the test files - hope they work?!

--- test/controllers/index.js
+++ test/controllers/index.js
@@ -1,18 +1,19 @@
-const chai = require('chai');
-const mocha = require('mocha');
-const chaiHttp = require('chai-http');
-const express = require('express');
+const chai = require('chai')
+const mocha = require('mocha')
+const chaiHttp = require('chai-http')
+const express = require('express')
 
-const server = express();
-chai.use(chaiHttp);
+const server = express() // should require('../../server/app')
+chai.use(chaiHttp)
 
 describe('index', function(){
     it('should render the index page on / GET', function(done){
         chai.request(server)
             .get('/')
             .end(function(err, res) {
-                res.should.have.status(200);
-                done();
+                res.should.have.status(200)
+                res.should.be.json
+                done()
             })
     })
 });
