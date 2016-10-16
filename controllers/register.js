3b7e5514292795f0a8da95c0719efb4953c2328c
Glyne J. Gittens <Jgit121@gmail.com>
2016-10-16 16:21:33 -0400

    added register controller

---
+++ controllers/register.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var router = express.Router()
+var models = require('../../models/user')
+
+router.get('/register', function(req, res){
+  res.render('register');
+})
+
+router.post('/register', function(req, res){
+    var user = models.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+    user.save(function(err){
+        if(err){
+          console.log(err)
+        }
+    })
+})
+
+module.exports = router;
