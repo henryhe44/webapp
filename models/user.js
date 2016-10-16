eda00c3c399f9139740398801c67411145bc92d4
Ming Yang <mgy209@nyu.edu>
2016-10-16 14:01:15 -0400

    Updated user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,43 @@
+'use strict'
+
+module.exports = function(sequelize, DataTypes) {
+	var User = sequelize.define('User', {
+		firstName: DataTypes.STRING,
+		lastName: DataTypes.STRING,
+		username: DataTypes.STRING,
+		email: DataTypes.STRING,
+		password: DataTypes.STRING
+	}, {
+		classMethods: {
+			associate: function(models) {
+				// creates associations between models
+			}
+		}
+	})
+	User.sync({force: true}).then(function() {
+		return User.create({
+			firstName: 'Donald',
+			lastName: 'Trump',
+			username: 'makemegreatagain',
+			email: 'makemegreatagain@god.com',
+			password: 'chinasux'
+		})
+	})
+	User.sync({force: true}).then(function() {
+		return User.create({
+			firstName: 'Hillary',
+			lastName: 'Clinton',
+			username: 'dontvoteforcrazy',
+			email: 'imthesaneone@usa.org',
+			password: 'iwillruletheworld'
+		})
+	})
+	User.findAll().then(function(users) {
+		console.log(users)
+	})
+		return User;
+}
+
 var Sequelize = require('sequelize')
 var connection = new Sequelize('arena_test', 'node', 'password', {
 	host: 'localhost',
