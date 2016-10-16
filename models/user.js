265d5e5f880373e8c30199e8c620f5c0283add9d
Ming Yang <mgy209@nyu.edu>
2016-10-16 15:17:05 -0400

    Fixed users.js

--- models/user.js
+++ models/user.js
@@ -37,36 +37,3 @@ module.exports = function(sequelize, DataTypes) {
 	})
 		return User;
 }
-
-var Sequelize = require('sequelize')
-var connection = new Sequelize('arena_test', 'node', 'password', {
-	host: 'localhost',
-	dialect: 'postgres'
-});
-
-connection
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
-
-var User = connection.define('user', {
-	firstName: {
-		type: Sequelize.STRING
-   },
-    lastName: {
-         type: Sequelize.STRING
-   },
-    username: {
-    	type: Sequelize.STRING
-    },
-    email: {
-    	type: Sequelize.STRING
-   }
-});
-connection.sync({
-	logging: console.log
-});
