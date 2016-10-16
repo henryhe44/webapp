1da55e47da9bad231a4424439c9fc6da830b7f7d
Glyne J. Gittens <Jgit121@gmail.com>
2016-10-16 15:48:27 -0400

    Added bodyParser to app.js

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const pug = require('pug');
-const express = require('express');
-const expressSession = require('express-session');
-const flash = require('connect-flash');
-const methodOverride = require('method-override');
-const path  = require('path');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const pug = require('pug')
+const express = require('express')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const path  = require('path')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+// parse application/json
+app.use(bodyParser.json())
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 // app.get('/', (req, res) => {
 //   res.send("This is the home page");
 // });
-app.use(require('./controllers/'));
+app.use(require('./controllers/'))
 
-app.listen(8000);
-module.exports = app;
+app.listen(8000)
+module.exports = app
