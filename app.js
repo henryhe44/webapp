07471af0f66c4744fdcc3766388ec49057fd80a8
Glyne J. Gittens <Jgit121@gmail.com>
2016-10-21 19:34:23 -0400

    Took old route requests out of app.js

--- app.js
+++ app.js
@@ -24,27 +24,10 @@ app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
-// app.get('/', (req, res) => {
-//   res.send("This is the home page");
-// });
+
 app.use(require('./controllers/'))
 
-// app.get('/register', function(req, res){
-//   res.render('register_home');
-// })
-// app.post('/register', function(req, res){
-//   models.User.sync().then(function() {
-//     return models.User.create({
-//       firstName: req.body.firstName,
-//       lastName: req.body.lastName,
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password
-//     })
-//   })
-//
-//   res.end("Success")
-// })
+
 
 app.listen(8000)
 module.exports = app
--- models/user.js
+++ models/user.js
@@ -34,7 +34,8 @@ module.exports = function(sequelize, DataTypes) {
 		})
 	})
 	User.findAll().then(function(users) {
-		console.log(users)
+		// console.log(users)
+		// Why are we doing this? lol -GG
 	})
 		return User;
 }
