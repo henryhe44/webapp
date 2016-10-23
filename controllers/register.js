b54970d83fdb6a7b6ba6023e99d80045c1cbdd37
Ming Yang <mgy209@nyu.edu>
2016-10-23 15:18:36 -0400

    Reset to working post for registration

--- controllers/register.js
+++ controllers/register.js
@@ -1,29 +1,23 @@
 var express = require('express')
 var router = express.Router()
-var models = require('../models/user')
+var models = require('../models')
 
 router.get('/register', function(req, res){
   res.render('register_home');
 })
 
 router.post('/register', function(req, res){
-    var user = models.User.create({
-      firstName:  req.body.firstName,
-      lastName:   req.body.lastName,
-      username:   req.body.username,
-      email:      req.body.email,
-      password:   req.body.password
-      /*
-      overwatch:  req.body.overwatch ? true : false,
-      league:     req.body.league    ? true : false,
-      starcraft:  req.body.starcraft ? true : false
-      */
-    })
-    user.save(function(err){
-        if(err){
-          console.log(err)
-        }
-    })
+   models.User.sync().then(function() {
+     return models.User.create({
+       firstName: req.body.firstName,
+       lastName: req.body.lastName,
+       username: req.body.username,
+       email: req.body.email,
+       password: req.body.password
+     })
+   })
+
+   res.end("Success")
 })
 
 module.exports = router;
