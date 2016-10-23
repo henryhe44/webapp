7f6540aa47437636dae053729cc8f5e44c62d1c5
Ming Yang <mgy209@nyu.edu>
2016-10-23 14:41:58 -0400

    Limited the games available during registration to 3

--- controllers/register.js
+++ controllers/register.js
@@ -8,11 +8,14 @@ router.get('/register', function(req, res){
 
 router.post('/register', function(req, res){
     var user = models.User.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
+      firstName:  req.body.firstName,
+      lastName:   req.body.lastName,
+      username:   req.body.username,
+      email:      req.body.email,
+      password:   req.body.password,
+      overwatch:  req.body.overwatch ? true : false,
+      league:     req.body.league    ? true : false,
+      starcraft:  req.body.starcraft ? true : false
     })
     user.save(function(err){
         if(err){
