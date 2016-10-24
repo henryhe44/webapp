5e53c5dd916829447db62267724d810da5fa759c
Henry He <henryhe44@users.noreply.github.com>
2016-10-23 22:20:46 -0400

    Aligned indentations

--- models/group.js
+++ models/group.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = function(sequelize, DataTypes) {
 	var Group = sequelize.define('Group', {
-    group_name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    //Perhaps announcement should be an array?
-    announcement: DataTypes.TEXT,
-    member_count: DataTypes.INTEGER
+		group_name: DataTypes.STRING,
+		description: DataTypes.TEXT,
+    		//Perhaps announcement should be an array?
+    		announcement: DataTypes.TEXT,
+    		member_count: DataTypes.INTEGER
 	}, {
 		classMethods: {
 			associate: function(models) {
