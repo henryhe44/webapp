4a60722ea6fe40b879bca23df3805bef2cd529c7
Henry He <henryhe44@users.noreply.github.com>
2016-10-23 22:19:50 -0400

    Added gamemode attribute and reformat indentations

--- models/game.js
+++ models/game.js
@@ -1,11 +1,12 @@
 'use strict'
 module.exports = function(sequelize, DataTypes) {
 	var Game = sequelize.define('Game', {
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    //Actually, should logo even be in the database?
-    logo: DataTypes.TEXT,
-    genre: DataTypes.STRING
+		name: DataTypes.STRING,
+		description: DataTypes.TEXT,
+		//Actually, should logo even be in the database?
+		logo: DataTypes.TEXT,
+		genre: DataTypes.STRING,
+		gamemode: DataTypes.ARRAY(DataTypes.STRING)
 	}, {
 		classMethods: {
 			associate: function(models) {
