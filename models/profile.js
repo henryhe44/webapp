da14663d6c04fd7d621569532fd3973b3ef9b6ec
Henry He <henryhe44@users.noreply.github.com>
2016-10-23 22:22:57 -0400

    Aligned Indentations

--- models/profile.js
+++ models/profile.js
@@ -1,22 +1,22 @@
 'use strict'
 module.exports = function(sequelize, DataTypes) {
 	var Profile = sequelize.define('Profile', {
-    aboutMe: DataTypes.TEXT,
+    		aboutMe: DataTypes.TEXT,
 		alias: DataTypes.STRING,
 
-    //Timezone should look something like +0500, -0100, if we go by UTC
+    		//Timezone should look something like +0500, -0100, if we go by UTC
 		//We can use the values to offset certain timestamps, refer to the sequelize documentation for more details
 		timezone: {
-      DataTypes.INTEGER,
-      validate: { min: -1200, max: 1400 }
-    },
+			DataTypes.INTEGER,
+			validate: { min: -1200, max: 1400 }
+		},
 
 		//We do need geolocation, but should we represent it as longtitude and latitude instead? hrm...
 		longtitude: DataTypes.INTEGER,
 		latitude: DataTypes.INTEGER,
 		games_played: DataTypes.ARRAY(DataTypes.STRING),
-    ignore_list: DataTypes.ARRAY(DataTypes.STRING),
-    friends_list: DataTypes.ARRAY(DataTypes.STRING):
+    		ignore_list: DataTypes.ARRAY(DataTypes.STRING),
+    		friends_list: DataTypes.ARRAY(DataTypes.STRING)
 	}, {
 		classMethods: {
 			associate: function(models) {
