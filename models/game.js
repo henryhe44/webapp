3bd345fdd0df0e81c912bb9c0e3460d56bbd076a
Henry He <henryhe44@users.noreply.github.com>
2016-10-30 14:36:55 -0400

    Changed game.js to reflect changes on ER Diagram

--- models/game.js
+++ models/game.js
@@ -3,10 +3,11 @@ module.exports = function(sequelize, DataTypes) {
 	var Game = sequelize.define('Game', {
 		name: DataTypes.STRING,
 		description: DataTypes.TEXT,
-		//Actually, should logo even be in the database?
-		logo: DataTypes.TEXT,
 		genre: DataTypes.STRING,
-		gamemode: DataTypes.ARRAY(DataTypes.STRING)
+		rank: DataTypes.ARRAY(DataTypes.STRING),
+		attitude: DataTypes.ARRAY(DataTypes.STRING),
+		t_m_system: DataTypes.STRING
+
 	}, {
 		classMethods: {
 			associate: function(models) {
