8cae54719a59f049dcdceb3e70072ac2785fcedc
Henry He <henryhe44@users.noreply.github.com>
2016-10-30 14:32:25 -0400

    Changed profile to reflect changes on ER Diagram

--- models/profile.js
+++ models/profile.js
@@ -2,21 +2,10 @@
 module.exports = function(sequelize, DataTypes) {
 	var Profile = sequelize.define('Profile', {
     		aboutMe: DataTypes.TEXT,
-		alias: DataTypes.STRING,
-
-    		//Timezone should look something like +0500, -0100, if we go by UTC
-		//We can use the values to offset certain timestamps, refer to the sequelize documentation for more details
-		timezone: {
-			DataTypes.INTEGER,
-			validate: { min: -1200, max: 1400 }
-		},
-
-		//We do need geolocation, but should we represent it as longtitude and latitude instead? hrm...
-		longtitude: DataTypes.INTEGER,
-		latitude: DataTypes.INTEGER,
-		games_played: DataTypes.ARRAY(DataTypes.STRING),
-    		ignore_list: DataTypes.ARRAY(DataTypes.STRING),
-    		friends_list: DataTypes.ARRAY(DataTypes.STRING)
+		country: DataTypes.STRING,
+		favored_genre: DataTypes.STRING,
+		//Alternatively, schedule can just be text for user to fill in, if complications arises
+		schedule: DataTypes.ARRAY(DataTypes.DATE)
 	}, {
 		classMethods: {
 			associate: function(models) {
