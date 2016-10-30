e8ec4ed811d836ad4a14f95094261a174747d545
Henry He <henryhe44@users.noreply.github.com>
2016-10-30 14:22:32 -0400

    Added friends_list attribute

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ module.exports = function(sequelize, DataTypes) {
 		firstName: DataTypes.STRING,
 		lastName: DataTypes.STRING,
 		username: DataTypes.STRING,
+		friends_list: DataTypes.ARRAY(DataTypes.STRING),
 		email: DataTypes.STRING,
 		password: DataTypes.STRING
 	}, {
