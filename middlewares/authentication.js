3d18a995933efd49d88223bf3220c8e95628f4c0
Glyne J. Gittens <Jgit121@gmail.com>
2016-11-06 15:26:12 -0500

    Added basic passport validation

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,6 +1,45 @@
-const passport  = require('passport');
-const User      = require('../models').User
+const passport      = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User          = require('../models').User
 
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    User.findOne({
+      where: {username}
+    }).then((user) => {
+      // if user isn't null, check the passwords
+      if(user){
+        // check passwords
+        if(password !== user.password){
+          return done(null, false, {message: "Passwords do not match"})
+        }
+      }else if (user == null){
+        return done(null, false, {message: "User does not exist"})
+      }
+      return done(null, user, {message: "Successfully logged in"})
+
+
+    })
+}))
+
+
+// Create Session Cookies
+passport.serializeUser(
+  (user, done) => {
+    done(null, user.id)
+})
+
+
+// Destroy Session Cookies on Logout
+passport.deserializeUser(
+  (id, done) =>{
+    User.findById(id,
+      (err, user) => {
+        done(err, user)
+    })
+
+})
 
 
 module.exports = passport;
