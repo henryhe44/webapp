62ee84ae57546762af7cbf6c9de18b847fd7e136
Henry He <henry.he44@qmail.cuny.edu>
2016-11-13 14:46:34 -0500

    removed a comma

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				isEmail: true,
+				isEmail: true
 			}
 		},
 		password: {
