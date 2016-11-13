42641a4b978488911a651d06acaef46497654063
Ming Yang <mgy209@nyu.edu>
2016-11-13 15:21:51 -0500

    Added require('passport') where necessary

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,12 +1,13 @@
-// const models = require('../models/');
-const express = require('express');
-const router = express.Router();
+// const models = require('../models/')
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
 
 router.get('/profile',
   passport.authenticate('local',
     { failureRedirect: '/login' }),
   function(req, res) {
-  
+
 })
 
 router.post('/profile', function(req, res){
