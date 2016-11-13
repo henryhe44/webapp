13b0686e27a64740e1f4973cd0f708e45416975d
Greszy <gregoryszy@gmail.com>
2016-11-13 13:57:41 -0500

    Update game.js

--- models/game.js
+++ models/game.js
@@ -1,19 +1,15 @@
-'use strict'
+$'use strict'
 module.exports = function(sequelize, DataTypes) {
 	var Game = sequelize.define('Game', {
-		name: DataTypes.STRING,
-		description: DataTypes.TEXT,
-		genre: DataTypes.STRING,
-		rank: DataTypes.ARRAY(DataTypes.STRING),
-		attitude: DataTypes.ARRAY(DataTypes.STRING),
-		t_m_system: DataTypes.STRING
+		game_name: DataTypes.STRING
 
 	}, {
 		classMethods: {
 			associate: function(models) {
-				// creates associations between models
+				/*Game.hasMany(models.User_Game_Info);
+                Game.hasMany(models.Gamer_Details);*/
 			}
 		}
-	})
+	});
 	return Game;
 }
